12d796e45395e64d32b613b093bf38a0bf7784d4
qRod <kioud@hotmail.fr>
2017-02-03 21:52:10 +0000

    cloudirany seulement en production

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,8 @@ class User < ActiveRecord::Base
   has_many :reservations
       
     has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-                    :default_url => "/assets/default_image.png",
-                    :storage => :cloudinary,
-                    :path => ':id/:style/:filename'                
+                      :default_url => "/assets/default_image.png"
+                                
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/ 
 
 end
