f7ed88dd5a1636753639b6279a0111ce03c613e8
qRod <kioud@hotmail.fr>
2017-02-03 20:17:54 +0000

    cloudinary en production

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
     
+    
     def destroy
         @photo = Photo.find(params[:id])
         room = @photo.room
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,10 @@
 class Photo < ActiveRecord::Base
   belongs_to :room
   
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-                    :storage => :cloudinary,
-                    :path => ':id/:style/:filename'
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :storage => :cloudinary, :if => lambda{ Rails.env.production?}
+  has_attached_file :image, :path => ':id/:style/:filename', :if => lambda{ Rails.env.production?}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ 
+ 
   
 end
