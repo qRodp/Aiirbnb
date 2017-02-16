f68b5770f2553e9e6049cd365b572a40d68626f5
qRod <kioud@hotmail.fr>
2017-02-16 18:40:25 +0000

    Mail de confimation heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,4 @@ class User < ActiveRecord::Base
                       :default_url => "/assets/default_image.png"            
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/ 
 
-protected
-    def confirmation_required?
-      false
-    end
-
 end
